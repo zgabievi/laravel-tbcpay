fcc7bb964608a1c075e49266f5ebd97c7139b1cb
Zura Gabievi <zura.gabievi@gmail.com>
2017-01-05 01:38:07 +0400

    Create TBCTrait.php

---
+++ src/TBCTrait.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Gabievi\TBC;
+
+trait TBCTrait {
+     /**
+     * @param string $query
+     *
+     * @return mixed
+     */
+    private function cURL($query)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
+        curl_setopt($curl, CURLOPT_VERBOSE, '1');
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, '0');
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, '0');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSLCERT, $this->cert_path);
+        curl_setopt($curl, CURLOPT_SSLKEY, $this->cert_path);
+        curl_setopt($curl, CURLOPT_SSLKEYPASSWD, $this->cert_pass);
+        curl_setopt($curl, CURLOPT_URL, $this->submit_url);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        return $result;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return string
+     */
+    private function queryString(array $data = [])
+    {
+        return http_build_query($data);
+    }
+
+    /**
+     * @param $string
+     *
+     * @return array
+     */
+    private function parse($string)
+    {
+        $params = explode(PHP_EOL, trim($string));
+        $result = [];
+
+        foreach ($params as $param) {
+            $parts = explode(':', $param);
+            $result[$parts[0]] = trim($parts[1]);
+        }
+
+        return $result;
+    }
+}
